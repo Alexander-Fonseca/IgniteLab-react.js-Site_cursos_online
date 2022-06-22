9bdfdc9a99a05587d023f6f53f97eecd72c447d1
Alexander Fonseca <motoleco@hotmail.com>
2022-06-21 22:13:59 -0300

    instalação e configuração do Apollo

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,8 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
+import { isConstValueNode } from "graphql"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons{
+      id
+      title
+    }
+  }
+`
+interface lesson{
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: lesson[] }>(GET_LESSONS_QUERY)
+
 
   return (
 
-    <h1 className="text-5xl font-bold text-violet-500">Hello Word</h1>
+    <ul>
+      {data?.lessons.map(lesson =>{
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
