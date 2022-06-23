bf2335f87f97d7a795172bbd51e016fac5bc2d39
Alexander Fonseca <motoleco@hotmail.com>
2022-06-23 13:38:46 -0300

    estilização da sidebar

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export function Sidebar() {
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-8">
         <Lesson />
         <Lesson />
         <Lesson />
