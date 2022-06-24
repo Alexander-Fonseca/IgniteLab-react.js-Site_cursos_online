e1e2b49f8678bfc88db43431e5e211288439fa76
Alexander Fonseca <motoleco@hotmail.com>
2022-06-24 15:46:01 -0300

    configurando as rotas do slug

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,13 +1,23 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import '@vime/core/themes/default.css';
 
-export function Video() {
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
 
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
-
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1 ">
