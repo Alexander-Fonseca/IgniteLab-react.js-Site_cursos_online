0aa485ef0888a28e65076b77697085b113f388ac
Alexander Fonseca <motoleco@hotmail.com>
2022-06-26 15:04:46 -0300

    finalizando tela login

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../components/Logo";
 export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center ">
-      <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
         <div className="max-w-[640px]">
           <Logo />
 
@@ -14,6 +14,25 @@ export function Subscribe() {
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
             </p>
         </div>
+
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input
+            className="bg-gray-900 rounded px-5 h-14" 
+            type="text" 
+            placeholder="Seu nome completo" />
+            <input
+            className="bg-gray-900 rounded px-5 h-14" 
+            type="email" 
+            placeholder="Digite seu e-mail" />
+
+            <button type="submit"
+            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors">
+              Garantir minha vaga
+            </button>
+          </form>
+          </div>
       </div>
       <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
 
