052333392acc2188b06ce44d1accae6d874c0af0
Alexander Fonseca <motoleco@hotmail.com>
2022-06-26 13:29:51 -0300

    css dos textos

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -7,10 +7,10 @@ export function Subscribe() {
         <div className="max-w-[640px]">
           <Logo />
 
-          <h1>
-            Construa uma aplicação completa, do zero, com React JS
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500" >React</strong>
           </h1>
-            <p>
+            <p className="mt-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
             </p>
         </div>
